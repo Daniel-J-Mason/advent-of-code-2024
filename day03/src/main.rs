e6450cac9a38d0ffe8441b482f4c3bfb9b8fb76f
Daniel-J-Mason <daniel.j.msn@gmail.com>
2024-12-03 04:39:08 -0800

    fix test name

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -51,7 +51,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn first() {
+    fn multiplication_sum_test() {
         assert_eq!(
             multiplication_sum(
                 &"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
